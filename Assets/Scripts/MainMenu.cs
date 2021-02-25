d0a968b8f54853541aa075266a97a5c26f1dd28f
solofhreaper <61039567+solofhreaper@users.noreply.github.com>
2021-02-24 22:06:50 -0500

    Main Menu Buttons Hooked up

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
-    public void PlayGame() {
-        SceneManager.LoadScene("Bus");
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+    	SceneManager.LoadScene("Bus");
     }
 
-    public void Quit() {
-        
+    public void QuitGame()
+    {
+    	Application.Quit();
     }
 }
